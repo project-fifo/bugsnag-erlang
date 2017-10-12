231c9c579b858fefab564ae915a21063f5a880f8
Heinz N. Gies <heinz@licenser.net>
2017-10-12 15:24:44 +0200

    Allow for app version stats

--- src/bugsnag_app.erl
+++ src/bugsnag_app.erl
@@ -4,16 +4,34 @@
 %% Application hooks
 -export([start/2, stop/1]).
 -ignore_xref([start/2, stop/1]).
+
+erlang_app_vsn(App) ->
+    AppA = list_to_atom(App),
+    Apps = application:loaded_applications(),
+    case lists:keyfind(AppA, 1, Apps) of
+        {AppA, _, Vsn} ->
+            list_to_binary(Vsn);
+        _ ->
+            <<"">>
+    end.
+
+app_vsn(App) ->
+    case application:get_env(bugsnag, app_version) of
+        {ok, Vsn} when Vsn =/= "" ->
+            list_to_binary(Vsn);
+        _ ->
+            erlang_app_vsn(App)
+    end.
+
 start(_Type, _Args) ->
     lager:info("Starting bugsnag notifier"),
-    {ok, ReleaseStateS} = application:get_env(bugsnag, release_state),
+    ReleaseStateS = application:get_env(bugsnag, release_state, ""),
     ReleaseState = list_to_binary(ReleaseStateS),
 
-    {ok, AppS} = application:get_env(bugsnag, app),
+    AppS = application:get_env(bugsnag, app, ""),
     App = list_to_binary(AppS),
 
-    {ok, AppVersionS} = application:get_env(bugsnag, app_version),
-    AppVersion = list_to_binary(AppVersionS),
+    AppVersion = app_vsn(AppS),
     ApiKey = case application:get_env(bugsnag, api_key) of
                  {ok, ApiKeyS} ->
                      list_to_binary(ApiKeyS);
